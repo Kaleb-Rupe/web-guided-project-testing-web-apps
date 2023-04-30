edd98fe63cf36096212c8a44d4eadcbfb0636965
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-04-30 02:35:39 -0400

    Completed Guided Project! Yay!

--- src/components/AnimalForm.js
+++ src/components/AnimalForm.js
@@ -6,35 +6,35 @@ const AnimalForm = ({ errors, touched, values }) => {
       species: "Lion",
       age: "12",
       notes: "",
-      id: 365
-    }
+      id: 365,
+    },
   ]);
   const [animal, setAnimal] = useState({ species: "", age: "", notes: "" });
 
   // Handle changes from form inputs
-  const handleChange = event => {
+  const handleChange = (event) => {
     setAnimal({ ...animal, [event.target.name]: event.target.value });
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     const newAnimal = {
       ...animal,
-      id: Date.now()
+      id: Date.now(),
     };
     setAnimals([...animals, newAnimal]);
   };
 
   return (
     <div className="animal-form">
-      <form onSubmit={event => handleSubmit(event)}>
+      <form onSubmit={(event) => handleSubmit(event)}>
         <label htmlFor="species">Species:</label>
         <input
           id="species"
           type="text"
           name="species"
           placeholder="species"
-          onChange={event => handleChange(event)}
+          onChange={(event) => handleChange(event)}
         />
         <label htmlFor="age">Age:</label>
         <input
@@ -42,7 +42,7 @@ const AnimalForm = ({ errors, touched, values }) => {
           type="text"
           name="age"
           placeholder="age"
-          onChange={event => handleChange(event)}
+          onChange={(event) => handleChange(event)}
         />
         <label htmlFor="notes">Notes:</label>
         <textarea
@@ -50,12 +50,12 @@ const AnimalForm = ({ errors, touched, values }) => {
           type="text"
           name="notes"
           placeholder="notes"
-          onChange={event => handleChange(event)}
+          onChange={(event) => handleChange(event)}
         />
         <button>Submit!</button>
       </form>
       <h4>Current Animals</h4>
-      {animals.map(animal => (
+      {animals.map((animal) => (
         <p key={animal.id}>{animal.species}</p>
       ))}
     </div>
